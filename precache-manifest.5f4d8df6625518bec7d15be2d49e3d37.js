6fdfd585b08b2f217e26758c82b58e485bd0eadf
Faisal Bin Ahmed <faisalbin@outlook.com>
2020-04-29 21:49:15 +0600

    Updates

---
+++ precache-manifest.5f4d8df6625518bec7d15be2d49e3d37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97acdbe141d5eb1000408900ffe9b271",
+    "url": "/namegamegenerator/index.html"
+  },
+  {
+    "revision": "f59c987d12f44081a6e5",
+    "url": "/namegamegenerator/static/css/main.4302f2d0.chunk.css"
+  },
+  {
+    "revision": "42d569f73a4a977ca9a2",
+    "url": "/namegamegenerator/static/js/2.7b10abcd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/namegamegenerator/static/js/2.7b10abcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59c987d12f44081a6e5",
+    "url": "/namegamegenerator/static/js/main.a9bcffdd.chunk.js"
+  },
+  {
+    "revision": "ee8f60ac8d2492bbe1cf",
+    "url": "/namegamegenerator/static/js/runtime-main.96c531fb.js"
+  }
+]);
